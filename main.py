1635e6314d7b05e3f2be079d3662f38e6589773f
Aman Sharma <aman2077sharma@gmail.com>
2025-08-01 17:31:35 +0530

    image resizing and and other rows

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/hariom.png")
+    st.image("images/hariom.png", width=800)
 
 with col2:
     st.title("Aman Sharma")
@@ -20,13 +20,20 @@ content2 = """ Below you can find some apps which i have created on my own in py
 feel free to contact me """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1,0.5,1])
 
 df = pandas.read_csv("data.csv")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
