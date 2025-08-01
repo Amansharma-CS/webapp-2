87a12d7d96ec73311eb6f741f01c1522db5ae419
Aman Sharma <aman2077sharma@gmail.com>
2025-08-01 17:05:38 +0530

    render the text titles

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
@@ -15,3 +16,17 @@ with col2:
     and my jiju name is gourav . 
      """
     st.info(content)
+content2 = """ Below you can find some apps which i have created on my own in python .
+feel free to contact me """
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
